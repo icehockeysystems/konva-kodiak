ec49a77a03529b537934ce301079fb522c101771
Nils Satterstrom <nils.satterstrom@gmail.com>
2023-12-18 14:31:51 -0600

    Add the stage

--- konva-kodiak-app/src/components/DrawingCanvas.js
+++ konva-kodiak-app/src/components/DrawingCanvas.js
@@ -1,55 +1,11 @@
-// src/components/DrawingCanvas.js
-
-import React, { useState } from "react";
-import { Stage, Layer, Line } from "react-konva";
-
-const DrawingCanvas = () => {
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [color, setColor] = useState("black");
-  const [points, setPoints] = useState([]);
-
-  const handleMouseDown = (e) => {
-    setIsDrawing(true);
-    setPoints([e.nativeEvent.offsetX, e.nativeEvent.offsetY]);
-  };
-
-  const handleMouseMove = (e) => {
-    if (!isDrawing) return;
-    setPoints((prevPoints) => [
-      ...prevPoints,
-      e.nativeEvent.offsetX,
-      e.nativeEvent.offsetY,
-    ]);
-  };
-
-  const handleMouseUp = () => {
-    setIsDrawing(false);
-    const newLine = new Line({
-      points,
-      stroke: color,
-      strokeWidth: 5,
-    });
-    setPoints([]); // Clear points for next draw
-    // Add new line to Konva layer
-    // ...
-  };
-
+import React from "react";
+import { Stage, Layer } from "react-konva";
+export default function DrawingCanvas() {
   return (
     <Stage width={600} height={400}>
       <Layer>
-        {/* ... Add existing drawn lines here ... */}
-        {isDrawing && (
-          <Line
-            points={points}
-            stroke={color}
-            strokeWidth={5}
-            tension={0.5}
-            // ... other line properties
-          />
-        )}
+        {/* Add your content to the layer here (for now, leave it empty) */}
       </Layer>
     </Stage>
   );
-};
-
-export default DrawingCanvas;
+}
