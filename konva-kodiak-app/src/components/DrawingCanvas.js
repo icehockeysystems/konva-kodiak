3681ef97fe11ff667b79a7d955a2aa28cb3181d3
Nils Satterstrom <nils.satterstrom@gmail.com>
2023-12-18 16:15:12 -0600

    add bkg image

--- konva-kodiak-app/src/components/DrawingCanvas.js
+++ konva-kodiak-app/src/components/DrawingCanvas.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Stage, Layer } from "react-konva";
+import { Stage, Image, Layer } from "react-konva";
+
 export default function DrawingCanvas() {
+  const svgContent = require("./IHS_bkg.png").default; // Import your SVG as a module
   return (
     <Stage width={600} height={400}>
       <Layer>
-        {/* Add your content to the layer here (for now, leave it empty) */}
+        <Image
+          image={svgContent}
+          x={0}
+          y={0}
+          width={600} // Match Stage width
+          height={400} // Match Stage height
+        />
       </Layer>
     </Stage>
   );
